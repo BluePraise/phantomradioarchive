7178fb14035f5dcf20a3112cee2170a0e8b2b28e
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-01-30 20:04:20 +0100

    play button now toggles

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 $(
-  function(){
-  var aud = $('audio')[0];
-  $('.play-pause').on('click', function(){
-  if (aud.paused) {
-    aud.play();
-    $('.play-pause').removeClass('icon-play');
-    $('.play-pause').addClass('icon-pause');
-  }
-  else {
-    aud.pause();
-    $('.play-pause').removeClass('icon-pause');
-    $('.play-pause').addClass('icon-play');
-  }
+function() {
+	$('.play-pause').on('click', function(e) {
+		e.preventDefault();
+		// get the list-item of the element that
+		// was clicked upon.
+		li = $(this).closest('li');
+		// find the audio of this li that was targeted
+		aud = li.find('audio');
 
-})
-  $('.next').on('click', function(){
-  aud.src = 'another audio source';
-})
-  aud.ontimeupdate = function(){
-    $('.progress').css('width', aud.currentTime / aud.duration * 100 + '%')
-  }
+		// if the audio is paused, play the audio
+		if (aud[0].paused) {
+			// if (findPause.length = 1) {
+	            aud[0].play();
+	            $(this).removeClass('icon-play');
+	            $(this).addClass('icon-pause');
+            // }
+        }
+        // else pause it.
+        else {
+            aud[0].pause();
+            $(this).removeClass('icon-pause');
+            $(this).addClass('icon-play');
+        }
+
+    });
+
 })
