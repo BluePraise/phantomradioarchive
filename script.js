46af75ba2bb772f39209528012ad9714fb1ff8bf
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-05 11:46:35 +0100

    styling of accordion

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(
-function() {
+$( document ).ready(function() {
 	$('.play-pause').on('click', function(e) {
 		e.preventDefault();
 		// get the list-item of the element that
@@ -26,23 +25,14 @@ function() {
     	}
     })
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
 
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-
+    $('.accordion--toggle').on('click', function(){
+        $(this).toggleClass('active');
+        $(this).next().toggle();
+        /* added a border with JS, because don't know how to do it in css */
+        $(this).next().css( 'border-top','1px solid' );
+    });
 })
+
+
+
