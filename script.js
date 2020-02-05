3ee6a4073673d3583916038b5425b45a4e482e5f
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-05 08:49:31 +0100

    background added and changes in type

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ function() {
 		// get the list-item of the element that
 		// was clicked upon.
 		li = $(this).closest('li');
-		// find the audio of this li that was targeted
+		// find the audio of the targeted li
 		aud = li.find('audio');
 
 		// if the audio is paused, play the audio
 		if (aud[0].paused) {
-			// if (findPause.length = 1) {
 	            aud[0].play();
 	            $(this).removeClass('icon-play');
 	            $(this).addClass('icon-pause');
-            // }
         }
         // else pause it.
         else {
